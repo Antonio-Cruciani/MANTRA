8a4f1a55e3288058d65ed9e6ee8543477589d464
antonio <antonio.cruciani@gssi.it>
2023-08-08 09:47:22 +0530

    road experiments

--- apx_tbc.jl
+++ apx_tbc.jl
@@ -1,23 +1,8 @@
 include("src/APXTBC.jl")
-
-
-datasets = ["01_hypertext.txt",
-"02_highschool.txt",
-"03_hospital_ward.txt",
-"04_college_msg.txt",
-"05_wiki_elections.txt",
-"06_highschool.txt",
-"07_digg_reply.txt",
-"08_infectious.txt",
-"09_primary_school.txt",
-"10_facebook_wall.txt",
-"11_slashdot_reply.txt",
-"12_highschool.txt",
-"13_topology.txt",
-"14_SMS.txt",
-"16_brain_100206_90.txt",
-"17_brain_100206_70.txt"]
 path = "graphs/"
+
+
+datasets = ["20_askubuntu.txt","22_superuser.txt","23_flickr_grow.txt"]
 
 
 println("Computing Ground Truth values for the prefix-foremost temporal betweenness")
@@ -27,22 +12,23 @@ for gn in datasets
     tg = load_temporal_graph(path*gn," ")
     print_stats(tg, graph_name= gn)
     flush(stdout)
-    result = threaded_temporal_prefix_foremost_betweenness(tg,0)
+    result = threaded_temporal_prefix_foremost_betweenness(tg,1000)
     nn = String(split(gn, ".t")[1])
 
     save_results(nn,"pfm",result[1],result[2])
 end
-
+#=
 
 println("Computing Groun Truth values for the shortest temporal betweenness")
 
+datasets = ["21_mathoverflow.txt"]
 
 for gn in datasets
 
     tg = load_temporal_graph(path*gn," ")
     print_stats(tg, graph_name= gn)
     flush(stdout)
-    result = threaded_temporal_shortest_betweenness(tg,0,false)
+    result = threaded_temporal_shortest_betweenness(tg,1000,false)
     nn = String(split(gn, ".t")[1])
 
     save_results(nn,"sh",result[1],result[2])
@@ -56,13 +42,13 @@ for gn in datasets
     tg = load_temporal_graph(path*gn," ")
     print_stats(tg, graph_name= gn)
     flush(stdout)
-    result = threaded_temporal_shortest_foremost_betweenness(tg,0,false)
+    result = threaded_temporal_shortest_foremost_betweenness(tg,1000,false)
     nn = String(split(gn, ".t")[1])
 
     save_results(nn,"sfm",result[1],result[2])
 end
-
-
+=#
+#=
 roads = ["18_venice.txt","19_bordeaux.txt"]
 
 println("Computing Ground Truth values for the prefix-foremost temporal betweenness")
@@ -107,3 +93,4 @@ for gn in roads
     save_results(nn,"sfm",result[1],result[2])
 end
 
+=#
--- progressive_tbc.jl
+++ progressive_tbc.jl
@@ -4,15 +4,7 @@ function print_samplig_stats(epsilon,delta,trials,ss)
     println(" ε = "*string(epsilon)*" δ = "*string(delta)*" #trials = "*string(trials)*" starting sample size/ub sample size "*string(ss))
     flush(stdout)
 end
-datasets = [
-"09_primary_school.txt",
-"10_facebook_wall.txt",
-"11_slashdot_reply.txt",
-"12_highschool.txt",
-"13_topology.txt",
-"14_SMS.txt",
-"16_brain_100206_90.txt",
-"17_brain_100206_70.txt"]
+datasets = ["18_venice.txt","19_bordeaux.txt"]
 
 path = "graphs/"
 
@@ -21,7 +13,8 @@ delta = 0.1
 trials = 10
 ss = 750
 geo = 1.2
-#=
+big_int = true
+
 for gn in datasets
     nn = String(split(gn, ".t")[1])
     tg = load_temporal_graph(path*gn," ")
@@ -33,7 +26,7 @@ for gn in datasets
         result = progressive_onbra_bernstein_prefix_foremost_temporal_betweenness(tg,ss,epsilon,delta,geo,0)
         save_results_progressive_sampling(nn,"b_onbra_pfm",result[1],result[2][end],result[4],ss,result[3])
     end
-    
+    #=
     println("Running Bernstein TRK")
     flush(stdout)
     for i in 1:trials
@@ -49,7 +42,7 @@ for gn in datasets
     end
     
 end
-
+=#
 for gn in datasets
     nn = String(split(gn, ".t")[1])
     tg = load_temporal_graph(path*gn," ")
@@ -58,10 +51,10 @@ for gn in datasets
     println("Running Bernstein ONBRA")
     flush(stdout)
     for i in 1:trials
-        result = progressive_onbra_bernstein_shortest_temporal_betweenness(tg,ss,epsilon,delta,geo,0,false)
+        result = progressive_onbra_bernstein_shortest_temporal_betweenness(tg,ss,epsilon,delta,geo,0,big_int)
         save_results_progressive_sampling(nn,"b_onbra_sh",result[1],result[2][end],result[4],ss,result[3])
     end
-    
+#=
     println("Running Bernstein TRK")
     flush(stdout)
     for i in 1:trials
@@ -86,7 +79,7 @@ for gn in datasets
     println("Running Bernstein ONBRA")
     flush(stdout)
     for i in 1:trials
-        result = progressive_onbra_bernstein_shortest_foremost_temporal_betweenness(tg,ss,epsilon,delta,geo,0,false)
+        result = progressive_onbra_bernstein_shortest_foremost_temporal_betweenness(tg,ss,epsilon,delta,geo,0,big_int)
         save_results_progressive_sampling(nn,"b_onbra_sfm",result[1],result[2][end],result[4],ss,result[3])
     end
     #=
