316dd5d01831b235029770304084b98a4a4e4cf9
antonio <antonio.cruciani@gssi.it>
2023-08-13 13:35:17 +0530

    new exp

--- progressive_vc.jl
+++ progressive_vc.jl
@@ -5,24 +5,6 @@ function print_samplig_stats(epsilon,delta,trials,ss)
     flush(stdout)
 end
 datasets = [
-"01_hypertext.txt",
-"02_highschool.txt",
-"03_hospital_ward.txt",
-"04_college_msg.txt",
-"05_wiki_elections.txt",
-"06_highschool.txt",
-"07_digg_reply.txt",
-"08_infectious.txt",
-"09_primary_school.txt",
-"10_facebook_wall.txt",
-"11_slashdot_reply.txt",
-"12_highschool.txt",
-"13_topology.txt",
-"14_SMS.txt",
-"16_brain_100206_90.txt",
-"17_brain_100206_70.txt",
-"18_venice.txt",
-"19_bordeaux.txt",
 "20_askubuntu.txt",
 "21_mathoverflow.txt",
 "22_superuser.txt",
@@ -40,14 +22,14 @@ k = 0
 for gn in datasets
     nn = String(split(gn, ".t")[1])
     tg = load_temporal_graph(path*gn," ")
-    vd = read_vertex_diameter(nn,"sh")
-    vc = compute_vapnik_chervonenkis_bound(vd,epsilon,delta)
-    print_samplig_stats(epsilon,delta,trials,vc)
+    #vd = read_vertex_diameter(nn,"sh")
+    #vc = compute_vapnik_chervonenkis_bound(vd,epsilon,delta)
+    #print_samplig_stats(epsilon,delta,trials,vc)
     print_stats(tg, graph_name= gn)
     println("Running W.UB. ONBRA")
     flush(stdout)
     for i in 1:trials
-        result = progressive_trk_prefix_foremost_temporal_betweenness(tg,epsilon,delta,k,10000,"ob",vd)
+        result = progressive_trk_prefix_foremost_temporal_betweenness(tg,epsilon,delta,k,10000,"ob")
         save_results_progressive_sampling(nn,"wub_onbra_pfm",result[1],result[4],result[6],vc,epsilon)
     end
     #=
