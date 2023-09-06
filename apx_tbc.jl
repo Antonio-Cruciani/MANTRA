b9bedfa74a2df18ab9db11517128031b30ff5302
antonio <antonio.cruciani@gssi.it>
2023-09-06 17:41:34 +0530

    some exp

--- apx_tbc.jl
+++ apx_tbc.jl
@@ -2,9 +2,29 @@ include("src/APXTBC.jl")
 path = "graphs/"
 
 
-datasets = ["21_mathoverflow.txt"]
+datasets = [
+    "16_brain_100206_90.txt",
+"17_brain_100206_70.txt",
+"01_hypertext.txt",
+"02_highschool.txt",
+"03_hospital_ward.txt",
+"04_college_msg.txt",
+"05_wiki_elections.txt",
+"06_highschool.txt",
+"07_digg_reply.txt",
+"08_infectious.txt",
+"09_primary_school.txt",
+"10_facebook_wall.txt",
+"11_slashdot_reply.txt",
+"12_highschool.txt",
+"13_topology.txt",
+"14_SMS.txt",
+"21_mathoverflow.txt",
+"20_askubuntu.txt",
+"22_superuser.txt"
+]
 
-#=
+
 println("Computing Ground Truth values for the prefix-foremost temporal betweenness")
 
 for gn in datasets
@@ -17,8 +37,6 @@ for gn in datasets
 
     save_results(nn,"pfm",result[1],result[2])
 end
-=#
-#=
 println("Computing Groun Truth values for the shortest temporal betweenness")
 
 
@@ -32,7 +50,7 @@ for gn in datasets
 
     save_results(nn,"sh",result[1],result[2])
 end
-=#
+
 println("Computing Groun Truth values for the shortest-foremost temporal betweenness")
 
 
@@ -47,7 +65,7 @@ for gn in datasets
     save_results(nn,"sfm",result[1],result[2])
 end
 
-#=
+
 roads = ["18_venice.txt","19_bordeaux.txt"]
 
 println("Computing Ground Truth values for the prefix-foremost temporal betweenness")
@@ -57,7 +75,7 @@ for gn in roads
     tg = load_temporal_graph(path*gn," ")
     print_stats(tg, graph_name= gn)
     flush(stdout)
-    result = threaded_temporal_prefix_foremost_betweenness(tg,0)
+    result = threaded_temporal_prefix_foremost_betweenness(tg,1000)
     nn = String(split(gn, ".t")[1])
 
     save_results(nn,"pfm",result[1],result[2])
@@ -72,7 +90,7 @@ for gn in roads
     tg = load_temporal_graph(path*gn," ")
     print_stats(tg, graph_name= gn)
     flush(stdout)
-    result = threaded_temporal_shortest_betweenness(tg,0,true)
+    result = threaded_temporal_shortest_betweenness(tg,1000,true)
     nn = String(split(gn, ".t")[1])
 
     save_results(nn,"sh",result[1],result[2])
@@ -86,10 +104,8 @@ for gn in roads
     tg = load_temporal_graph(path*gn," ")
     print_stats(tg, graph_name= gn)
     flush(stdout)
-    result = threaded_temporal_shortest_foremost_betweenness(tg,0,true)
+    result = threaded_temporal_shortest_foremost_betweenness(tg,1000,true)
     nn = String(split(gn, ".t")[1])
 
     save_results(nn,"sfm",result[1],result[2])
 end
-
-=#
