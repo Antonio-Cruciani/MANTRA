5cdc9d051d2b645c500f5ac06a66841491d0b8b1
antonio <antonio.cruciani@gssi.it>
2023-09-10 01:32:48 +0530

    new stuff

--- progressive_exp.jl
+++ progressive_exp.jl
@@ -25,7 +25,7 @@ datasets = [
 "13_topology.txt",
 "14_SMS.txt",
 "18_venice.txt",
-"19_bordeaux.txt"
+"19_bordeaux.txt",
 "21_mathoverflow.txt",
 "20_askubuntu.txt",
 "22_superuser.txt"
@@ -40,17 +40,73 @@ datasets = [
 =#
 path = "graphs/"
 sample_step = 96
-epsilon_list = [0.05,0.01,0.005]
+epsilon_list = [0.01]
 #epsilon = 0.05
 delta = 0.1
 trials = 10
-sample_list = [750,1000,1500]
+sample_list = [1000]
 #starting_ss = 750
 geo = 1.2
 big_int = false
 topt = "pfm"
 algo = "ob"
 k = 0
+for i in 1:lastindex(epsilon_list)
+    epsilon = epsilon_list[i]
+    starting_ss = sample_list[i]
+    for gn in datasets
+        nn = String(split(gn, ".t")[1])
+        tg = load_temporal_graph(path*gn," ")
+        print_samplig_stats(epsilon,delta,trials,starting_ss)
+        print_stats(tg, graph_name= gn)
+        println("Running WUB")
+        flush(stdout)
+        for i in 1:trials
+            result = progressive_wub(tg,epsilon,delta,k,10000,big_int,algo,topt,-1,100,sample_step)
+            save_results_progressive_sampling(nn,"wub_"*algo*"_"*topt,result[1],result[4],result[6],starting_ss,epsilon)
+        end
+        println("Runnin CMCERA")
+        flush(stdout)
+        for i in 1:trials
+            result = progressive_cmcera(tg,epsilon,delta,0,big_int,algo,topt,2.0,sample_step)
+            save_results_progressive_sampling(nn,"cm_"*algo*"_"*topt,result[1],result[2],result[4],starting_ss,epsilon)
+        end
+
+    end
+end
+
+
+epsilon_list = [0.005]
+sample_list = [1500]
+
+
+datasets = [
+"16_brain_100206_90.txt",
+"17_brain_100206_70.txt",
+"01_hypertext.txt",
+"02_highschool.txt",
+"03_hospital_ward.txt",
+"04_college_msg.txt",
+"05_wiki_elections.txt",
+"06_highschool.txt",
+"07_digg_reply.txt",
+"08_infectious.txt",
+"09_primary_school.txt",
+"10_facebook_wall.txt",
+"11_slashdot_reply.txt",
+"12_highschool.txt",
+"13_topology.txt",
+"14_SMS.txt",
+"18_venice.txt",
+"19_bordeaux.txt",
+"21_mathoverflow.txt",
+
+
+]
+
+
+
+
 for i in 1:lastindex(epsilon_list)
     epsilon = epsilon_list[i]
     starting_ss = sample_list[i]
@@ -81,6 +137,37 @@ for i in 1:lastindex(epsilon_list)
     end
 end
 
+
+
+datasets = [
+"20_askubuntu.txt",
+"22_superuser.txt"
+
+]
+
+for i in 1:lastindex(epsilon_list)
+    epsilon = epsilon_list[i]
+    starting_ss = sample_list[i]
+    for gn in datasets
+        nn = String(split(gn, ".t")[1])
+        tg = load_temporal_graph(path*gn," ")
+        print_samplig_stats(epsilon,delta,trials,starting_ss)
+        print_stats(tg, graph_name= gn)
+        println("Running WUB")
+        flush(stdout)
+        for i in 1:trials
+            result = progressive_wub(tg,epsilon,delta,k,10000,big_int,algo,topt,-1,100,sample_step)
+            save_results_progressive_sampling(nn,"wub_"*algo*"_"*topt,result[1],result[4],result[6],starting_ss,epsilon)
+        end
+        println("Runnin CMCERA")
+        flush(stdout)
+        for i in 1:trials
+            result = progressive_cmcera(tg,epsilon,delta,0,big_int,algo,topt,2.0,sample_step)
+            save_results_progressive_sampling(nn,"cm_"*algo*"_"*topt,result[1],result[2],result[4],starting_ss,epsilon)
+        end
+
+    end
+end
 #SH
 
 topt = "sh"
