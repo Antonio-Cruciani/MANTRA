8a57bcb77eaeb4fd6aa0ba947593e41d1bc88bb2
antonio <antonio.cruciani@gssi.it>
2023-09-14 00:20:41 +0530

    other changes

--- progressive_exp.jl
+++ progressive_exp.jl
@@ -215,11 +215,99 @@ for i in 1:lastindex(epsilon_list)
 end
 
 
-epsilon_list = [0.01,0.005]
+epsilon_list = [0.01]
 
-sample_list = [1000,1500]
+sample_list = [1000]
 topt = "sh"
 datasets = [
+    "03_hospital_ward.txt",
+    "04_college_msg.txt",
+    "05_wiki_elections.txt",
+    "06_highschool.txt",
+    "07_digg_reply.txt",
+    "08_infectious.txt",
+    "09_primary_school.txt",
+    "10_facebook_wall.txt",
+    "11_slashdot_reply.txt",
+    "12_highschool.txt",
+    "14_SMS.txt",
+    "21_mathoverflow.txt",
+    "20_askubuntu.txt",
+    "22_superuser.txt"
+]
+
+
+for i in 1:lastindex(epsilon_list)
+    epsilon = epsilon_list[i]
+    starting_ss = sample_list[i]
+    for gn in datasets
+        nn = String(split(gn, ".t")[1])
+        tg = load_temporal_graph(path*gn," ")
+        print_samplig_stats(epsilon,delta,trials,starting_ss)
+        print_stats(tg, graph_name= gn)
+        
+        println("Running WUB")
+        flush(stdout)
+        for i in 1:trials
+            result = progressive_wub(tg,epsilon,delta,k,10000,big_int,algo,topt,-1,100,sample_step)
+            save_results_progressive_sampling(nn,"wub_"*algo*"_"*topt,result[1],result[4],result[6],starting_ss,epsilon)
+        end
+        println("Runnin CMCERA")
+        flush(stdout)
+        for i in 1:trials
+            result = progressive_cmcera(tg,epsilon,delta,0,big_int,algo,topt,2.0,sample_step)
+            save_results_progressive_sampling(nn,"cm_"*algo*"_"*topt,result[1],result[2],result[4],starting_ss,epsilon)
+        end
+
+    end
+end
+datasets = [
+    "16_brain_100206_90.txt",
+    "17_brain_100206_70.txt",
+    "01_hypertext.txt",
+    "02_highschool.txt",
+    "03_hospital_ward.txt",
+    "04_college_msg.txt",
+    "05_wiki_elections.txt",
+    "06_highschool.txt",
+    "07_digg_reply.txt",
+    "08_infectious.txt",
+    "09_primary_school.txt",
+    "10_facebook_wall.txt",
+    "11_slashdot_reply.txt",
+    "12_highschool.txt",
+    "14_SMS.txt",
+    "21_mathoverflow.txt",
+    "20_askubuntu.txt",
+    "22_superuser.txt"
+]
+for i in 1:lastindex(epsilon_list)
+    epsilon = epsilon_list[i]
+    starting_ss = sample_list[i]
+    for gn in datasets
+        nn = String(split(gn, ".t")[1])
+        tg = load_temporal_graph(path*gn," ")
+        print_samplig_stats(epsilon,delta,trials,starting_ss)
+        print_stats(tg, graph_name= gn)
+        println("Running Bernstein")
+        flush(stdout)
+        for i in 1:trials
+            result = progressive_bernstein(tg,starting_ss,epsilon,delta,geo,1000, big_int,algo,topt)
+            save_results_progressive_sampling(nn,"b_"*algo*"_"*topt,result[1],result[2][end],result[4],starting_ss,result[3])
+        end
+
+    end
+end
+
+epsilon_list = [0.005]
+
+sample_list = [1500]
+topt = "sh"
+datasets = [
+    "16_brain_100206_90.txt",
+    "17_brain_100206_70.txt",
+    "01_hypertext.txt",
+    "02_highschool.txt",
     "03_hospital_ward.txt",
     "04_college_msg.txt",
     "05_wiki_elections.txt",
@@ -279,8 +367,6 @@ for i in 1:lastindex(epsilon_list)
 
     end
 end
-
-
 # SFM
 
 big_int = false
