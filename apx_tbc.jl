d347a62797f7ac010bcef7cf79b9f3d38bfd8578
antonio <antonio.cruciani@gssi.it>
2023-09-29 10:30:49 +0530

    new

--- apx_tbc.jl
+++ apx_tbc.jl
@@ -11,21 +11,7 @@ end
 
 
 println("Computing Groun Truth values for the shortest-foremost temporal betweenness")
-datasets = [
-    "18_venice.txt",
-    "19_bordeaux.txt",
-]
-for gn in datasets
 
-    tg = load_temporal_graph(path*gn," ")
-    print_stats(tg, graph_name= gn)
-    flush(stdout)
-    result = threaded_temporal_prefix_foremost_betweenness(tg,1000)
-    nn = String(split(gn, ".t")[1])
-    save_results(nn,"pfm",result[1],result[2])
-
-
-end
 datasets = [
 
     "14_SMS.txt",
