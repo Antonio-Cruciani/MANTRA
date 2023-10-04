2ccd9fe421cfa4b472e992e481ead76d47e903b9
antonio <antonio.cruciani@gssi.it>
2023-10-04 12:15:49 +0530

    exp prog

---
+++ progressive_big_int.jl
@@ -0,0 +1,67 @@
+include("src/APXTBC.jl")
+
+function print_samplig_stats(epsilon,delta,trials,ss)
+    println(" ε = "*string(epsilon)*" δ = "*string(delta)*" #trials = "*string(trials)*" starting sample size/ub sample size "*string(ss))
+    flush(stdout)
+end
+function clean_gc()
+    GC.gc()
+end
+# PFM
+#epsilon_list = [0.005]
+#sample_list = [1500]
+epsilon_list = [0.1,0.07,0.05,0.01]
+sample_list = [100,350,750,1000]
+path = "graphs/"
+sample_step = 32
+delta = 0.1
+trials = 5
+geo = 1.2
+big_int = false
+#topt = "pfm"
+algo = "ob"
+k = 0
+
+epsilon_list = [0.1,0.07,0.05,0.01]
+sample_list = [100,350,750,1000]
+datasets = [
+    "18_venice.txt",
+    "19_bordeaux.txt"
+]
+big_int = true
+topt = "sh"
+
+for i in 1:lastindex(epsilon_list)
+    epsilon = epsilon_list[i]
+    starting_ss = sample_list[i]
+    for gn in datasets
+        nn = String(split(gn, ".t")[1])
+        tg = load_temporal_graph(path*gn," ")
+        print_samplig_stats(epsilon,delta,trials,starting_ss)
+        print_stats(tg, graph_name= gn)
+        println("Running Bernstein")
+        flush(stdout)
+        for i in 1:trials
+            result = progressive_bernstein(tg,starting_ss,epsilon,delta,geo,1000, big_int,algo,topt)
+            save_results_progressive_sampling(nn,"b_"*algo*"_"*topt,result[1],result[2][end],result[4],starting_ss,result[3])
+            clean_gc()
+        end
+        println("Running WUB")
+        flush(stdout)
+        for i in 1:trials
+            result = progressive_wub(tg,epsilon,delta,k,10000,big_int,algo,topt,-1,100,sample_step)
+            save_results_progressive_sampling(nn,"wub_"*algo*"_"*topt,result[1],result[4],result[6],starting_ss,epsilon)
+            clean_gc()
+        end
+        println("Runnin CMCERA")
+        flush(stdout)
+        for i in 1:trials
+            result = progressive_cmcera(tg,epsilon,delta,0,big_int,algo,topt,2.0,sample_step)
+            save_results_progressive_sampling(nn,"cm_"*algo*"_"*topt,result[1],result[2],result[4],starting_ss,epsilon)
+            clean_gc()
+        end
+
+    end
+end
+
+
