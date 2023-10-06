fe52e935af13df237ec2182c68d1038376f8116e
antonio <antonio.cruciani@gssi.it>
2023-10-06 19:10:38 +0530

    new stuff

--- progressive_exp.jl
+++ progressive_exp.jl
@@ -75,7 +75,33 @@ for i in 1:lastindex(epsilon_list)
     end
 end
 
+upperbound_sample = "rho"
+vc_upper_bound = false
 
+for i in 1:lastindex(epsilon_list)
+    epsilon = epsilon_list[i]
+    starting_ss = sample_list[i]
+    for gn in datasets
+        nn = String(split(gn, ".t")[1])
+        tg = load_temporal_graph(path*gn," ")
+        print_samplig_stats(epsilon,delta,trials,starting_ss)
+        print_stats(tg, graph_name= gn)
+        println("Running Bernstein")
+        flush(stdout)
+        for i in 1:trials
+            result = progressive_bernstein(tg,epsilon,delta,geo, big_int,algo,topt,vc_upper_bound)
+            save_results_progressive_sampling(nn,"b_"*algo*"_"*topt*"_"*upperbound_sample,result[1],result[2][end],result[4],starting_ss,result[3])
+            clean_gc()
+        end
+        println("Running WUB")
+        flush(stdout)
+        for i in 1:trials
+            result = progressive_wub(tg,epsilon,delta,k,big_int,algo,topt,vc_upper_bound,geo)
+            save_results_progressive_sampling(nn,"wub_"*algo*"_"*topt*"_"*upperbound_sample,result[1],result[4],result[6],starting_ss,epsilon)
+            clean_gc()
+        end
+    end
+end
 
 
 
