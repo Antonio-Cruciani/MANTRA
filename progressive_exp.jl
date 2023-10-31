9c5da377b44db5cd664f18cdebdc486038eecdb8
antonio <antonio.cruciani@gssi.it>
2023-10-31 11:20:47 +0530

    new stuff

--- progressive_exp.jl
+++ progressive_exp.jl
@@ -137,32 +137,53 @@ end
 clean_gc()
 =#
 # SFM
-epsilon_list = [0.1,0.07,0.05,0.01]
-sample_list = [100,350,750,1000]
+epsilon_list = [0.01]
+sample_list = [1000]
 topt = "sfm"
 datasets = [
-    "16_brain_100206_90.txt",
-    "17_brain_100206_70.txt",
-    "01_hypertext.txt",
-    "02_highschool.txt",
-    "03_hospital_ward.txt",
-    "04_college_msg.txt",
-    "05_wiki_elections.txt",
-    "06_highschool.txt",
-    "07_digg_reply.txt",
-    "08_infectious.txt",
-    "09_primary_school.txt",
-    "10_facebook_wall.txt",
-    "11_slashdot_reply.txt",
-    "12_highschool.txt",
-    "13_topology.txt",
-    "14_SMS.txt",
-    "20_askubuntu.txt",
-    "21_mathoverflow.txt",
-    "22_superuser.txt"
-]
+    "21_mathoverflow.txt"]
 
 topt = "sfm"
+trials = 3
+for i in 1:lastindex(epsilon_list)
+    epsilon = epsilon_list[i]
+    starting_ss = sample_list[i]
+    for gn in datasets
+        nn = String(split(gn, ".t")[1])
+        tg = load_temporal_graph(path*gn," ")
+        print_samplig_stats(epsilon,delta,trials,starting_ss)
+        print_stats(tg, graph_name= gn)
+        println("Running WUB")
+        flush(stdout)
+        for i in 1:trials
+            result = progressive_wub(tg,epsilon,delta,k,big_int,algo,topt,vc_upper_bound,geo)
+            save_results_progressive_sampling(nn,"wub_"*algo*"_"*topt*"_"*upperbound_sample,result[1],result[4],result[6],starting_ss,epsilon)
+            clean_gc()
+        end
+    end
+end
+trials = 10
+for i in 1:lastindex(epsilon_list)
+    epsilon = epsilon_list[i]
+    starting_ss = sample_list[i]
+    for gn in datasets
+        nn = String(split(gn, ".t")[1])
+        tg = load_temporal_graph(path*gn," ")
+        print_samplig_stats(epsilon,delta,trials,starting_ss)
+        print_stats(tg, graph_name= gn)
+        println("Runnin CMCERA")
+        flush(stdout)
+        for i in 1:trials
+            result = progressive_cmcera(tg,epsilon,delta,0,big_int,algo,topt,2.0,sample_step)
+            save_results_progressive_sampling(nn,"cm_"*algo*"_"*topt,result[1],result[2],result[4],starting_ss,epsilon)
+            clean_gc()
+        end
+
+    end
+end
+datasets = [
+    "22_superuser.txt"
+]
 
 for i in 1:lastindex(epsilon_list)
     epsilon = epsilon_list[i]
@@ -196,6 +217,5 @@ for i in 1:lastindex(epsilon_list)
 
     end
 end
-
 
 
