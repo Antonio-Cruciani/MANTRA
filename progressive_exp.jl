71f570cf734429e58d7424f684dd012276ad9164
antonio <antonio.cruciani@gssi.it>
2023-11-03 14:02:45 +0530

    new stuff

--- progressive_exp.jl
+++ progressive_exp.jl
@@ -25,47 +25,6 @@ topt = "sh"
 upperbound_sample = "var"
 
 
-epsilon_list = [0.1]
-sample_list = [100]
-datasets = [
-
-"22_superuser.txt"
-]
-
-for i in 1:lastindex(epsilon_list)
-    epsilon = epsilon_list[i]
-    starting_ss = sample_list[i]
-    for gn in datasets
-        nn = String(split(gn, ".t")[1])
-        tg = load_temporal_graph(path*gn," ")
-        print_samplig_stats(epsilon,delta,trials,starting_ss)
-        print_stats(tg, graph_name= gn)
-        println("Running Bernstein")
-        flush(stdout)
-        for i in 1:trials
-            result = progressive_bernstein(tg,epsilon,delta,geo, big_int,algo,topt,vc_upper_bound)
-            save_results_progressive_sampling(nn,"b_"*algo*"_"*topt*"_"*upperbound_sample,result[1],result[2][end],result[4],starting_ss,result[3])
-            clean_gc()
-        end
-        println("Running WUB")
-        flush(stdout)
-        for i in 1:trials
-            result = progressive_wub(tg,epsilon,delta,k,big_int,algo,topt,vc_upper_bound,geo)
-            save_results_progressive_sampling(nn,"wub_"*algo*"_"*topt*"_"*upperbound_sample,result[1],result[4],result[6],starting_ss,epsilon)
-            clean_gc()
-        end
-        #=
-        println("Running c-MC ERA")
-        flush(stdout)
-        for i in 1:trials
-            result = progressive_cmcera(tg,epsilon,delta,0,big_int,algo,topt)
-            save_results_progressive_sampling(nn,"cm_"*algo*"_"*topt,result[1],result[2],result[4],starting_ss,epsilon)
-            clean_gc()
-        end
-        =#
-    end
-end
-
 epsilon_list = [0.07,0.05,0.01]
 sample_list = [350,750,1000]
 datasets = [
