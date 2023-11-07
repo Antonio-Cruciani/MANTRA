8187faf07a3dfeae31ac6a6fa99e9af715194591
antonio <antonio.cruciani@gssi.it>
2023-11-07 11:20:54 +0530

    new comm

--- progressive_exp.jl
+++ progressive_exp.jl
@@ -25,28 +25,9 @@ topt = "pfm"
 upperbound_sample = "vc"
 
 
-epsilon_list = [0.1,0.07,0.05,0.01]
-sample_list = [100,350,750,1000]
+epsilon_list = [0.01]
+sample_list = [1000]
 datasets = [
-"16_brain_100206_90.txt",
-"17_brain_100206_70.txt",
-"01_hypertext.txt",
-"02_highschool.txt",
-"03_hospital_ward.txt",
-"04_college_msg.txt",
-"05_wiki_elections.txt",
-"06_highschool.txt",
-"07_digg_reply.txt",
-"08_infectious.txt",
-"09_primary_school.txt",
-"10_facebook_wall.txt",
-"11_slashdot_reply.txt",
-"12_highschool.txt",
-"13_topology.txt",
-"14_SMS.txt",
-"21_mathoverflow.txt",
-"18_venice.txt",
-"19_bordeaux.txt",
 "20_askubuntu.txt",
 "22_superuser.txt"
 ]
