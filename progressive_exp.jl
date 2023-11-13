a10a03176358f08bfc922efc76004f16a8d22623
antonio <antonio.cruciani@gssi.it>
2023-11-13 11:38:10 +0530

    new stuff

--- progressive_exp.jl
+++ progressive_exp.jl
@@ -16,13 +16,13 @@ sample_step = 32
 delta = 0.1
 trials = 1
 geo = 1.2
-big_int = true
-vc_upper_bound = true
+big_int = false
+vc_upper_bound = false
 #topt = "pfm"
 algo = "ob"
 k = 0
-topt = "sh"
-upperbound_sample = "vc"
+topt = "sfm"
+upperbound_sample = "var"
 
 
 epsilon_list = [0.01]
@@ -47,10 +47,9 @@ datasets = [
 "14_SMS.txt",
 "21_mathoverflow.txt",
 "20_askubuntu.txt",
-"22_superuser.txt"
 ]=#
 datasets = [
-    "18_venice.txt",
+    "22_superuser.txt"
 ]
 for i in 1:lastindex(epsilon_list)
     epsilon = epsilon_list[i]
@@ -60,31 +59,7 @@ for i in 1:lastindex(epsilon_list)
         tg = load_temporal_graph(path*gn," ")
         print_samplig_stats(epsilon,delta,trials,starting_ss)
         print_stats(tg, graph_name= gn)
-        
-
-        println("Running c-MC ERA")
-        flush(stdout)
-        for i in 1:trials
-            result = progressive_cmcera(tg,epsilon,delta,big_int,algo,topt,vc_upper_bound)
-            save_results_progressive_sampling(nn,"cm_"*algo*"_"*topt*"_"*upperbound_sample,result[1],result[2],result[4],starting_ss,epsilon)
-            clean_gc()
-        end
-      
-        
-    end
-end
-datasets = [
-    "19_bordeaux.txt"
-]
-for i in 1:lastindex(epsilon_list)
-    epsilon = epsilon_list[i]
-    starting_ss = sample_list[i]
-    for gn in datasets
-        nn = String(split(gn, ".t")[1])
-        tg = load_temporal_graph(path*gn," ")
-        print_samplig_stats(epsilon,delta,trials,starting_ss)
-        print_stats(tg, graph_name= gn)
-        
+        #=
         println("Running Bernstein")
         flush(stdout)
         for i in 1:trials
@@ -92,6 +67,7 @@ for i in 1:lastindex(epsilon_list)
             save_results_progressive_sampling(nn,"b_"*algo*"_"*topt*"_"*upperbound_sample,result[1],result[2][end],result[4],starting_ss,result[3])
             clean_gc()
         end
+        =#
         println("Running WUB")
         flush(stdout)
         for i in 1:trials
@@ -99,7 +75,7 @@ for i in 1:lastindex(epsilon_list)
             save_results_progressive_sampling(nn,"wub_"*algo*"_"*topt*"_"*upperbound_sample,result[1],result[4],result[6],starting_ss,epsilon)
             clean_gc()
         end
-   
+        #=
         println("Running c-MC ERA")
         flush(stdout)
         for i in 1:trials
@@ -107,10 +83,11 @@ for i in 1:lastindex(epsilon_list)
             save_results_progressive_sampling(nn,"cm_"*algo*"_"*topt*"_"*upperbound_sample,result[1],result[2],result[4],starting_ss,epsilon)
             clean_gc()
         end
-      
+      =#
         
     end
 end
+
 #=
 upperbound_sample = "rho"
 vc_upper_bound = false
