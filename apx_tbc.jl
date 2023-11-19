7184b561b400b743345fa6e572883f6af29ceeea
antonio <antonio.cruciani@gssi.it>
2023-11-19 11:52:21 +0530

    new exp for superuser

--- apx_tbc.jl
+++ apx_tbc.jl
@@ -15,7 +15,7 @@ println("Computing Groun Truth values for the shortest-foremost temporal between
 
 datasets = [
 
-"21_mathoverflow.txt"
+"22_superuser.txt"
 ]
 
 
@@ -26,17 +26,18 @@ for gn in datasets
     print_stats(tg, graph_name= gn)
     flush(stdout)
    
-    
+    #=
     result = threaded_temporal_shortest_betweenness(tg,1000,false,true)
     nn = String(split(gn, ".t")[1])
 
     save_results(nn,"sh",result[1],result[2])
     clean_gc()
-    #=
+    =#
+    
     result =threaded_temporal_shortest_foremost_betweenness(tg,1000,false,true)
     nn = String(split(gn, ".t")[1])
 
     save_results(nn,"sfm",result[1],result[2])
     clean_gc()
-    =#
+    
 end
