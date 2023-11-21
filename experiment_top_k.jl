4864cec097a3f4bd259f13f6a6a9e452dec19e40
antonio <antonio.cruciani@gssi.it>
2023-11-21 15:42:56 +0530

    new experiment sfm

--- experiment_top_k.jl
+++ experiment_top_k.jl
@@ -16,18 +16,35 @@ path = "graphs/"
 delta = 0.1
 geo = 1.2
 k_list = [25]
-big_int = true
+big_int = false
 #topt = "pfm"
 algo = "ob"
-topt = "sh"
+topt = "sfm"
 force_gc = true
 
 
 trials = 5
 
 datasets = [
-    "18_venice.txt",
-    "19_bordeaux.txt"
+    "16_brain_100206_90.txt",
+    "17_brain_100206_70.txt",
+    "01_hypertext.txt",
+    "02_highschool.txt",
+    "03_hospital_ward.txt",
+    "04_college_msg.txt",
+    "05_wiki_elections.txt",
+    "06_highschool.txt",
+    "07_digg_reply.txt",
+    "08_infectious.txt",
+    "09_primary_school.txt",
+    "10_facebook_wall.txt",
+    "11_slashdot_reply.txt",
+    "12_highschool.txt",
+    "13_topology.txt",
+    "14_SMS.txt",
+    "21_mathoverflow.txt",
+    "20_askubuntu.txt",
+    "22_superuser.txt"
     ]
 
 upp_bound_list = ["var"]
