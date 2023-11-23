501107d4b91d58ed601fd669187936a14066ce14
antonio <antonio.cruciani@gssi.it>
2023-11-24 01:03:12 +0530

    new vers

--- experiment_onbra.jl
+++ experiment_onbra.jl
@@ -14,7 +14,7 @@ end
 path = "graphs/"
 sample_step = 32
 delta = 0.1
-trials = 5
+trials = 4
 geo = 1.2
 big_int = false
 vc_upper_bound = true
@@ -25,8 +25,41 @@ topt = "pfm"
 upperbound_sample = "vc"
 
 
-epsilon_list = [0.1,0.07,0.05,0.01]
-sample_list = [100,350,750,1000]
+epsilon_list = [0.05]
+sample_list = [750]
+datasets = ["23_wiki_talk.txt"]
+
+
+
+for i in 1:lastindex(epsilon_list)
+    epsilon = epsilon_list[i]
+    starting_ss = sample_list[i]
+    for gn in datasets
+        nn = String(split(gn, ".t")[1])
+        tg = load_temporal_graph(path*gn," ")
+        print_samplig_stats(epsilon,delta,trials,starting_ss)
+        print_stats(tg, graph_name= gn)
+        
+        println("Running Bernstein")
+        flush(stdout)
+        for i in 1:trials
+            result = progressive_bernstein(tg,epsilon,delta,geo, big_int,algo,topt,true)
+            save_results_progressive_sampling(nn,"ONBRA_b_"*algo*"_"*topt*"_"*upperbound_sample,result[1],result[2][end],result[4],starting_ss,result[3])
+            clean_gc()
+        end
+   
+        
+      
+        
+    end
+end
+
+
+trials = 5
+
+epsilon_list = [0.01]
+sample_list = [1000]
+
 
 datasets = [
 "16_brain_100206_90.txt",
