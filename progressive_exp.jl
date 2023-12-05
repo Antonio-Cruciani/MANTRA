b873bd1b147ff2cee88082d2021f95cb71bc3f55
antonio <antonio.cruciani@gssi.it>
2023-12-05 12:35:25 +0530

    new exp 0.005

--- progressive_exp.jl
+++ progressive_exp.jl
@@ -14,7 +14,7 @@ end
 path = "graphs/"
 sample_step = 32
 delta = 0.1
-trials = 4
+trials = 5
 geo = 1.2
 big_int = false
 vc_upper_bound = false
@@ -87,7 +87,43 @@ datasets = [
     "19_bordeaux.txt"
 ]
 =#
-datasets = ["21_mathoverflow.txt"]
+datasets = ["21_mathoverflow.txt",
+"22_superuser.txt"]
+
+for i in 1:lastindex(epsilon_list)
+    epsilon = epsilon_list[i]
+    starting_ss = sample_list[i]
+    for gn in datasets
+        nn = String(split(gn, ".t")[1])
+        tg = load_temporal_graph(path*gn," ")
+        print_samplig_stats(epsilon,delta,trials,starting_ss)
+        print_stats(tg, graph_name= gn)
+        #=
+        println("Running Bernstein")
+        flush(stdout)
+        for i in 1:trials
+            result = progressive_bernstein(tg,epsilon,delta,geo, big_int,algo,topt,true,true)
+            save_results_progressive_sampling(nn,"b_"*algo*"_"*topt*"_"*upperbound_sample,result[1],result[2][end],result[4],starting_ss,result[3])
+            clean_gc()
+        end
+        
+        =#
+        
+        println("Running c-MC ERA")
+        flush(stdout)
+        for i in 1:trials
+            result = progressive_cmcera(tg,epsilon,delta,big_int,algo,topt,false,1.2,-1,2.0,true)
+            save_results_progressive_sampling(nn,"cm_"*algo*"_"*topt,result[1],result[2],result[4],starting_ss,epsilon)
+            clean_gc()
+        end
+        
+        
+        
+    end
+end
+
+trials = 4
+datasets = ["20_askubuntu.txt"]
 
 for i in 1:lastindex(epsilon_list)
     epsilon = epsilon_list[i]
