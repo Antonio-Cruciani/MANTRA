a302ed9eee1ae2c1f98f82623c8ddbb55ab52e04
antonio <antonio.cruciani@gssi.it>
2024-01-21 23:41:13 +0530

    new rtb

--- progressive_rtb.jl
+++ progressive_rtb.jl
@@ -32,29 +32,7 @@ upperbound_sample = "vc"
 #sample_list = [1500]
 
 
-datasets = [
-    "18_venice.txt",
-    "19_bordeaux.txt",
-    "16_brain_100206_90.txt",
-    "17_brain_100206_70.txt",
-    "01_hypertext.txt",
-    "02_highschool.txt",
-    "03_hospital_ward.txt",
-    "04_college_msg.txt",
-    "05_wiki_elections.txt",
-    "06_highschool.txt",
-    "07_digg_reply.txt",
-    "08_infectious.txt",
-    "09_primary_school.txt",
-    "10_facebook_wall.txt",
-    "11_slashdot_reply.txt",
-    "12_highschool.txt",
-    "13_topology.txt",
-    "14_SMS.txt",
-    "21_mathoverflow.txt",
-    "20_askubuntu.txt",
-    "22_superuser.txt"
-]
+
 
 #=
 
@@ -168,12 +146,120 @@ datasets = [
 "19_bordeaux.txt"
 ]
 =#
+datasets = [
+    "19_bordeaux.txt"
+    
+]
+trials =2
+topt = "pfm"
+big_int = false
+epsilon_list = [0.007]
+sample_list = [1350]
+for i in 1:lastindex(epsilon_list)
+    epsilon = epsilon_list[i]
+    starting_ss = sample_list[i]
+    for gn in datasets
+        nn = String(split(gn, ".t")[1])
+        tg = load_temporal_graph(path*gn," ")
+        print_samplig_stats(epsilon,delta,trials,starting_ss)
+        print_stats(tg, graph_name= gn)
+
+        
+        println("Running c-MC ERA")
+        flush(stdout)
+        for i in 1:trials
+            result = progressive_cmcera(tg,epsilon,delta,big_int,algo,topt,false,1.2,-1,2.0,true)
+            save_results_progressive_sampling(nn,"cm_"*algo*"_"*topt,result[1],result[2],result[4],starting_ss,epsilon)
+            clean_gc()
+        end
+        
+        
+        
+    end
+end
+trials =5
+datasets = [
+    
+    "16_brain_100206_90.txt",
+    "17_brain_100206_70.txt",
+    "01_hypertext.txt",
+    "02_highschool.txt",
+    "03_hospital_ward.txt",
+    "04_college_msg.txt",
+    "05_wiki_elections.txt",
+    "06_highschool.txt",
+    "07_digg_reply.txt",
+    "08_infectious.txt",
+    "09_primary_school.txt",
+    "10_facebook_wall.txt",
+    "11_slashdot_reply.txt",
+    "12_highschool.txt",
+    "13_topology.txt",
+    "14_SMS.txt",
+    "21_mathoverflow.txt",
+    "20_askubuntu.txt",
+    "22_superuser.txt"
+]
+
+topt = "pfm"
+big_int = false
+#epsilon_list = [0.1,0.07,0.05,0.01,0.007,0.005,0.001]
+#sample_list = [100,350,750,1000,1350,1500,2000]
+#
+epsilon_list = [0.007]
+sample_list = [1350]
+for i in 1:lastindex(epsilon_list)
+    epsilon = epsilon_list[i]
+    starting_ss = sample_list[i]
+    for gn in datasets
+        nn = String(split(gn, ".t")[1])
+        tg = load_temporal_graph(path*gn," ")
+        print_samplig_stats(epsilon,delta,trials,starting_ss)
+        print_stats(tg, graph_name= gn)
+
+        
+        println("Running c-MC ERA")
+        flush(stdout)
+        for i in 1:trials
+            result = progressive_cmcera(tg,epsilon,delta,big_int,algo,topt,false,1.2,-1,2.0,true)
+            save_results_progressive_sampling(nn,"cm_"*algo*"_"*topt,result[1],result[2],result[4],starting_ss,epsilon)
+            clean_gc()
+        end
+        
+        
+        
+    end
+end
+
+datasets = [
+    "18_venice.txt",
+    "19_bordeaux.txt",
+    "16_brain_100206_90.txt",
+    "17_brain_100206_70.txt",
+    "01_hypertext.txt",
+    "02_highschool.txt",
+    "03_hospital_ward.txt",
+    "04_college_msg.txt",
+    "05_wiki_elections.txt",
+    "06_highschool.txt",
+    "07_digg_reply.txt",
+    "08_infectious.txt",
+    "09_primary_school.txt",
+    "10_facebook_wall.txt",
+    "11_slashdot_reply.txt",
+    "12_highschool.txt",
+    "13_topology.txt",
+    "14_SMS.txt",
+    "21_mathoverflow.txt",
+    "20_askubuntu.txt",
+    "22_superuser.txt"
+]
 
 
 topt = "pfm"
 big_int = false
-epsilon_list = [0.1,0.07,0.05,0.01,0.007,0.005,0.001]
-sample_list = [100,350,750,1000,1350,1500,2000]
+epsilon_list = [0.005,0.001]
+sample_list = [1500,2000]
 for i in 1:lastindex(epsilon_list)
     epsilon = epsilon_list[i]
     starting_ss = sample_list[i]
