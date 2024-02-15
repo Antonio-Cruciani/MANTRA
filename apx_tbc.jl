5ffdff531db7f527ccbc8b32267e5c1632122122
antonio <antonio.cruciani@gssi.it>
2024-02-15 13:01:22 +0530

    new exp sh

--- apx_tbc.jl
+++ apx_tbc.jl
@@ -16,8 +16,7 @@ println("Computing Groun Truth values for the shortest-foremost temporal between
 
 
 datasets = [
-"24_flickr_grow.txt"
-
+    "23_wiki_talk.txt"
 
 ]
 
@@ -28,21 +27,22 @@ for gn in datasets
     tg = load_temporal_graph(path*gn," ")
     print_stats(tg, graph_name= gn)
     flush(stdout)
-   
+      #=
     result = threaded_temporal_prefix_foremost_betweenness(tg,1000,false)
     nn = String(split(gn, ".t")[1])
 
     save_results(nn,"pfm",result[1],result[2])
     clean_gc()
+    =#
     
-   #=
-    result = threaded_temporal_shortest_betweenness(tg,1000,false,true)
+
+    result = threaded_temporal_shortest_betweenness(tg,1000,false,false)
     nn = String(split(gn, ".t")[1])
 
     save_results(nn,"sh",result[1],result[2])
     clean_gc()
    
-    
+       #=
     result =threaded_temporal_shortest_foremost_betweenness(tg,1000,false,true)
     nn = String(split(gn, ".t")[1])
 
