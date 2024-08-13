220b49aa4b7a4190fd4f2cad4fec2ba23d7e6061
Antonio <antonio.cruciani@gssi.it>
2024-08-13 14:04:02 +0530

    new exp

--- compute_mantra.jl
+++ compute_mantra.jl
@@ -28,7 +28,7 @@ delta =0.1
 trials = 10
 global big_int = false
 geo = 1.2
-algo = "trk"
+algo = "rtb"
 println("Computing absolute (ε,δ)-Approximation of the (*)-temporal betweenness using MANTRA")
 println("Suggestion : Go and grab a (quick) coffee ;)")
 flush(stdout)
