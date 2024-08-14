cf1ee1ad116a16acc7759ae546b35788c6e0914b
Antonio <antonio.cruciani@gssi.it>
2024-08-14 11:59:52 +0530

    new exp

--- compute_mantra.jl
+++ compute_mantra.jl
@@ -9,6 +9,74 @@ datasets = ["00_workplace.txt"]
 epsilon_list = [0.1,0.07]
 sample_list = [100,350]
 =#
+epsilon_list = [0.01]
+sample_list = [1000]
+datasets = [
+    "10_superuser.txt",
+]
+trials = 1
+global big_int = false
+geo = 1.2
+algo = "rtb"
+println("Computing absolute (ε,δ)-Approximation of the (*)-temporal betweenness using MANTRA")
+println("Suggestion : Go and grab a (quick) coffee ;)")
+flush(stdout)
+println("Computing (ε,δ)-Approximation for the prefix-foremost temporal betweenness")
+flush(stdout)
+topt = "pfm"
+for i in 1:lastindex(epsilon_list)
+    epsilon = epsilon_list[i]
+    starting_ss = sample_list[i]
+    for gn in datasets
+        nn = String(split(gn, ".t")[1])
+        tg = load_temporal_graph(path*gn," ")
+        print_samplig_stats(epsilon,delta,trials,starting_ss)
+        print_stats(tg, graph_name= gn)
+        println("Running Progressive MANTRA")
+        flush(stdout)
+        for i in 1:trials
+            result = progressive_mantra(tg,epsilon,delta, big_int,algo,topt)
+            save_results_progressive_sampling(nn,"cm_"*algo*"_"*topt,result[1],result[2],result[4],starting_ss,epsilon)
+            clean_gc()
+        end
+    end
+end
+
+
+epsilon_list = [0.01]
+sample_list = [1000]
+datasets = [
+    "11_wiki_talk.txt"
+]
+trials = 10
+global big_int = false
+geo = 1.2
+#algo = "rtb"
+println("Computing absolute (ε,δ)-Approximation of the (*)-temporal betweenness using MANTRA")
+println("Suggestion : Go and grab a (quick) coffee ;)")
+flush(stdout)
+println("Computing (ε,δ)-Approximation for the prefix-foremost temporal betweenness")
+flush(stdout)
+topt = "pfm"
+for i in 1:lastindex(epsilon_list)
+    epsilon = epsilon_list[i]
+    starting_ss = sample_list[i]
+    for gn in datasets
+        nn = String(split(gn, ".t")[1])
+        tg = load_temporal_graph(path*gn," ")
+        print_samplig_stats(epsilon,delta,trials,starting_ss)
+        print_stats(tg, graph_name= gn)
+        println("Running Progressive MANTRA")
+        flush(stdout)
+        for i in 1:trials
+            result = progressive_mantra(tg,epsilon,delta, big_int,algo,topt)
+            save_results_progressive_sampling(nn,"cm_"*algo*"_"*topt,result[1],result[2],result[4],starting_ss,epsilon)
+            clean_gc()
+        end
+    end
+end
+
+
 datasets = [
     "01_college_msg.txt",
     "02_digg_reply.txt",
@@ -22,8 +90,8 @@ datasets = [
     "10_superuser.txt",
     "11_wiki_talk.txt"
 ]
-epsilon_list = [0.1,0.07,0.05,0.01,0.007,0.005,0.001]
-sample_list = [100,350,750,1000,1350,1500,2000]
+epsilon_list = [0.007,0.005,0.001]
+sample_list = [1350,1500,2000]
 delta =0.1
 trials = 10
 global big_int = false
@@ -52,7 +120,8 @@ for i in 1:lastindex(epsilon_list)
         end
     end
 end
-
+epsilon_list = [0.1,0.07,0.05,0.01,0.007,0.005,0.001]
+sample_list = [100,350,750,1000,1350,1500,2000]
 println("Computing (ε,δ)-Approximation for the shortest temporal betweenness")
 flush(stdout)
 topt = "sh"
