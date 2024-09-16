717c12b63ce2dcec3dc88811d3b81c1abaa5ad4f
Antonio <antonio.cruciani@gssi.it>
2024-09-16 11:30:20 +0300

    new version

--- compute_mantra.jl
+++ compute_mantra.jl
@@ -11,61 +11,22 @@ sample_list = [100,350]
 =#
 
 
-epsilon_list = [0.01]
-sample_list = [1000]
-datasets = [
-    "11_wiki_talk.txt"
-]
-delta = 0.1
-trials = 10
-global big_int = false
-geo = 1.2
-algo = "trk"
-println("Computing absolute (ε,δ)-Approximation of the (*)-temporal betweenness using MANTRA")
-println("Suggestion : Go and grab a (quick) coffee ;)")
-flush(stdout)
-println("Computing (ε,δ)-Approximation for the prefix-foremost temporal betweenness")
-flush(stdout)
-topt = "pfm"
-for i in 1:lastindex(epsilon_list)
-    epsilon = epsilon_list[i]
-    starting_ss = sample_list[i]
-    for gn in datasets
-        nn = String(split(gn, ".t")[1])
-        tg = load_temporal_graph(path*gn," ")
-        print_samplig_stats(epsilon,delta,trials,starting_ss)
-        print_stats(tg, graph_name= gn)
-        println("Running Progressive MANTRA")
-        flush(stdout)
-        for i in 1:trials
-            result = progressive_mantra(tg,epsilon,delta, big_int,algo,topt)
-            save_results_progressive_sampling(nn,"cm_"*algo*"_"*topt,result[1],result[2],result[4],starting_ss,epsilon)
-            clean_gc()
-        end
-    end
-end
+
 
 
 datasets = [
-    "01_college_msg.txt",
-    "02_digg_reply.txt",
-    "03_slashdot_reply.txt",
-    "04_facebook_wall.txt",
-    "05_topology.txt",
-    "06_bordeaux.txt",
-    "07_mathoverflow.txt",
-    "08_SMS.txt",
+    
     "09_askubuntu.txt",
     "10_superuser.txt",
     "11_wiki_talk.txt"
 ]
-epsilon_list = [0.007,0.005,0.001]
-sample_list = [1350,1500,2000]
+epsilon_list = [0.001]
+sample_list = [2000]
 delta =0.1
 trials = 10
 global big_int = false
 geo = 1.2
-#algo = "ob"
+algo = "ob"
 println("Computing absolute (ε,δ)-Approximation of the (*)-temporal betweenness using MANTRA")
 println("Suggestion : Go and grab a (quick) coffee ;)")
 flush(stdout)
